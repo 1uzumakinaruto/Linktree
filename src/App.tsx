6b763c6fd51ff79656590b369bd44f70b053344c
Bharat Bhushan <bbjee2019@gmail.com>
2024-08-16 17:52:16 +0530

    changed for production

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Hero from './components/Hero';
 import AboutMe from './components/AboutMe'; // Create this component
